a65c5e63741f37f43a77eb79eae743d2a66af447
xuwd11 <31458899+xuwd11@users.noreply.github.com>
2018-03-21 01:31:32 -0400

    added modules

---
+++ modules/IO.py
@@ -0,0 +1,14 @@
+import pickle
+
+class IO:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        
+    def to_pickle(self, obj):
+        with open(self.file_name, 'wb') as output:
+            pickle.dump(obj, output, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    def read_pickle(self):
+        with open(self.file_name, 'rb') as input_:
+            obj = pickle.load(input_)
+        return obj
